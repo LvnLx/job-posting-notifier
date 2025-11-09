2432f23a421b5502ad714fd4fe4db21f6260d0d3
LvnLx <Levin.Leesemann@gmail.com>
2025-11-08 18:03:13 -0600

    Update Spotify client exlusions

--- src/main/java/com/lvnlx/job/posting/notifier/client/SpotifyClient.java
+++ src/main/java/com/lvnlx/job/posting/notifier/client/SpotifyClient.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Service
 public class SpotifyClient extends Client<SpotifyJob> {
-    static final List<String> exclusions = List.of("manager", "senior machine", "staff machine", "research", "senior data", "senior product");
+    static final List<String> exclusions = List.of("manager", "senior machine", "staff machine", "staff", "research", "senior data", "senior product");
     static final List<String> inclusions = List.of("");
 
     SpotifyClient(BigQuery bigQuery, HttpService httpService, NotificationService notificationService) throws InterruptedException {
